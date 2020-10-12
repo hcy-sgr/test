4d74dd354e6bf735e43ea4e08b34178d96a40fc9
hcy-sgr <997379502@qq.com>
2020-10-12 22:27:34 +0800

    改过了

--- src/App.vue
+++ src/App.vue
@@ -8,9 +8,8 @@
     <h3>master的h3</h3>
     <h2>dev的h2</h2>
     <h3>dev的h3</h3>
-    <div>10/12的修改</div>
-    <div>10/12的修改2</div>
-    <span>dev的span</span>
+    <div>绝改动</div>
+    <div>第二个改动</div>
     <router-view/>
   </div>
 </template>
